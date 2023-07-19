ff417b45f4349311db811660c6a3460cced95d2f
Artem Synytsin <sh1nodartem@gmail.com>
2023-07-19 16:20:02 +0300

    add benefits section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
 import { SelectedPage } from "@/shared/types";
 
 function App() {
@@ -30,9 +31,8 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
-      <Home
-        setSelectedPage={setSelectedPage}
-      />
+      <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
     </div>
   )
 }
